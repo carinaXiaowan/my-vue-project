c4650115dd66c17fb3e19d2c59e957fa769a668b
陈晓婉 <chenxiaowan@chenxiaowandeMacBook-Pro.local>
2022-08-23 10:00:55 +0800

    fix:校验

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,23 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended", //关闭vue文件命名的校验
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier'
   ],
-  rules: {
-    "no-unused-vars": "off",
-    "vue/multi-word-component-names": 0,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-    ],
-    "prettier/prettier": [
-      "error",
-      {},
-      {
-        usePrettierrc: false,
-      },
-    ],
+  env: {
+    'vue/setup-compiler-macros': true
   },
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 12
   },
-};
+  rules: {
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
+  }
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,31 @@
 module.exports = {
-  // 一行的字符数，如果超过会进行换行，默认为80
-  printWidth: 80,
-  // 一个tab代表几个空格数，默认为80
-  tabWidth: 2,
-  // 是否使用tab进行缩进，默认为false，表示用空格进行缩减
-  useTabs: false,
-  // 字符串是否使用单引号，默认为false，使用双引号
-  singleQuote: true,
-  // 行位是否使用分号，默认为true
-  semi: false,
-  // 是否使用尾逗号，有三个可选值"<none|es5|all>"
-  trailingComma: "none",
-  // 对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
-  bracketSpacing: true,
-};
+  // 定制格式化要求
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: {
+        parser: 'json'
+      }
+    }
+  ],
+  printWidth: 100, // 一行最多 100 字符
+  tabWidth: 2, // 使用 4 个空格缩进
+  semi: false, // 行尾需要有分号
+  singleQuote: true, // 使用单引号而不是双引号
+  useTabs: false, // 用制表符而不是空格缩进行
+  quoteProps: 'as-needed', // 仅在需要时在对象属性两边添加引号
+  jsxSingleQuote: false, // 在 JSX 中使用单引号而不是双引号
+  trailingComma: 'none', // 末尾不需要逗号
+  bracketSpacing: true, // 大括号内的首尾需要空格
+  bracketSameLine: false, // 将多行 HTML（HTML、JSX、Vue、Angular）元素反尖括号需要换行
+  arrowParens: 'always', // 箭头函数，只有一个参数的时候，也需要括号 avoid
+  rangeStart: 0, // 每个文件格式化的范围是开头-结束
+  rangeEnd: Infinity, // 每个文件格式化的范围是文件的全部内容
+  requirePragma: false, // 不需要写文件开头的 @prettier
+  insertPragma: false, // 不需要自动在文件开头插入 @prettier
+  proseWrap: 'preserve', // 使用默认的折行标准 always
+  htmlWhitespaceSensitivity: 'css', // 根据显示样式决定 html 要不要折行
+  vueIndentScriptAndStyle: false, //（默认值）对于 .vue 文件，不缩进 <script> 和 <style> 里的内容
+  endOfLine: 'lf', // 换行符使用 lf 在Linux和macOS以及git存储库内部通用\n
+  embeddedLanguageFormatting: 'auto' //（默认值）允许自动格式化内嵌的代码块
+}
