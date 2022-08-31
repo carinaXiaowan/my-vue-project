c4f507102eaa5247459ee6594a6b0e49a46aecb3
陈晓婉 <chenxiaowan@chenxiaowandeMacBook-Pro.local>
2022-08-31 08:56:43 +0800

    fix:echart

--- src/hooks/web/useEchart/index.ts
+++ src/hooks/web/useEchart/index.ts
@@ -1,6 +1,19 @@
-export const useEchart = ()=>{
-  const data = {
-    name:'carina'
+import type { Ref } from 'vue';
+import {unref} from 'vue'
+import type { EChartsOption } from 'echarts';
+import echarts from './echarts'
+
+export const useEchart = (elRef: Ref<HTMLDivElement>, theme: 'light' | 'dark' | 'default' = 'default')=>{
+  let chartInstance: echarts.ECharts | null = null;
+  const initCharts = (t = theme)=>{
+    const el = unref(elRef);
+    if (!el || !unref(el)) {
+      return;
+    }
+    chartInstance = echarts.init(el, t);
   }
-  return data
+  
+  return {
+
+  }
 }
