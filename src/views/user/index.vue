7d3756f92a3ef4ddd01aa81fc65ecf74e8ac5726
陈晓婉 <chenxiaowan@chenxiaowandeMacBook-Pro.local>
2022-09-01 13:07:56 +0800

    fix:列表数据

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,35 +1,24 @@
 <script setup lang="ts">
 // 导入文件
-import { aboutPinia } from '@/stores/about';
-import { storeToRefs } from 'pinia';
-import { useRouter } from 'vue-router';
+import { unref, ref, toRef, toRefs } from 'vue';
 
 // 初始变量
-const router = useRouter(); //路由
-const aboutPiniaData = aboutPinia(); //状态数据
-const { message } = storeToRefs(aboutPiniaData);
-// 逻辑部分
-// 更新消息
-const handleUpdateMessage = () => {
-  console.info('我是按钮点击');
-  aboutPiniaData.setNewMessage('我是更新消息的');
+const ref_data = ref(1);
+const normal_data = 2;
+const obj = {
+  name: 'carina',
+  age: 10
 };
-// 重置消息
-const handleResetMessage = () => {
-  aboutPiniaData.$reset();
-};
-// 返回首页
-const handleJumpToHome = () => {
-  router.push('/');
-};
+console.info('ref_data', unref(ref_data));
+console.info('ref_data', ref_data.value);
+console.info('normal_data', unref(normal_data));
+console.info('obj', obj);
+console.info('obj1', toRef(obj, 'name'));
+console.info('obj2', toRefs(obj));
 </script>
 <template>
   <div class="user-wrap">
     <h1>This is an about page</h1>
-    <div>{{ message }}</div>
-    <a-button type="primary" style="margin-top: 20px;" @click="handleUpdateMessage">更新消息</a-button>
-    <a-button type="default" style="margin-top: 20px;" @click="handleResetMessage">重置消息</a-button>
-    <a-button type="default" style="margin-top: 20px;" @click="handleJumpToHome">返回首页</a-button>
   </div>
 </template>
 
