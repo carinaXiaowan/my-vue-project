1ea8cf3ad70f172764eb4143659fbc3b35eaa67d
陈晓婉 <chenxiaowan@chenxiaowandeMacBook-Pro.local>
2022-09-02 15:00:18 +0800

    fix:修改数据

---
+++ src/components/base/ReTable/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  import {ref, reactive, defineProps} from 'vue'
+  defineProps(['dataSource','columns','loading','pagination'])
+</script>
+<template>
+  <slot name="otherBth" ></slot>
+  <a-table 
+      :dataSource="dataSource"
+      :columns="columns"
+      :loading="loading"
+      :pagination="pagination">
+      <template #[item]="data" v-for="item in Object.keys($slots)" :key="item">
+        <!-- 插槽后面-slot:[item]如果是动态渲染插槽需要使用这种方法 -->
+        <!-- slots是插槽的名字，遍历生成插槽 -->
+        <slot :name="item" v-bind="data || {}"></slot>
+      </template>
+  </a-table>
+</template>
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,24 +1,23 @@
 <script setup lang="ts">
 // 导入文件
-import { unref, ref, toRef, toRefs } from 'vue';
+import { unref, ref, toRef, toRefs,reactive } from 'vue';
+import ReTable from '@/components/base/ReTable/index.vue'
+import {dataSource,columns} from './schema'
 
-// 初始变量
-const ref_data = ref(1);
-const normal_data = 2;
-const obj = {
-  name: 'carina',
-  age: 10
-};
-console.info('ref_data', unref(ref_data));
-console.info('ref_data', ref_data.value);
-console.info('normal_data', unref(normal_data));
-console.info('obj', obj);
-console.info('obj1', toRef(obj, 'name'));
-console.info('obj2', toRefs(obj));
+// 逻辑处理
 </script>
 <template>
   <div class="user-wrap">
-    <h1>This is an about page</h1>
+    <ReTable :dataSource="dataSource" :columns="columns" >
+      <template #title>
+        列表数据
+      </template>
+      <template #bodyCell="{ column }">
+        <template v-if="column.key === 'operation'">
+          <a-switch checked-children="启用" un-checked-children="禁用" />
+        </template>
+      </template>
+    </ReTable>
   </div>
 </template>
 
