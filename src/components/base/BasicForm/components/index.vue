7c1fc92cf8050b20d9c7582a0f253d8eb987499a
陈晓婉 <chenxiaowan@chenxiaowandeMacBook-Pro.local>
2022-09-07 10:57:16 +0800

    feat:自定义表单

---
+++ src/components/base/BasicForm/components/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  import type { Component, defineComponent } from 'vue';
+  import {
+    Input,
+    Select,
+    Radio,
+    Checkbox,
+    AutoComplete,
+    Cascader,
+    DatePicker,
+    InputNumber,
+    Switch,
+    TimePicker,
+    TreeSelect,
+    Slider,
+    Rate,
+    Divider
+  } from 'ant-design-vue';
+  import type { ComponentType } from './interface';
+
+// 组件配置
+const componentMap = new Map<ComponentType, Component>();
+componentMap.set('a-input', Input);
+componentMap.set('a-select', Select);
+componentMap.set('Radio', Radio);
+componentMap.set('Checkbox', Checkbox);
+componentMap.set('AutoComplete', AutoComplete);
+componentMap.set('Cascader', Cascader);
+componentMap.set('DatePicker', DatePicker);
+componentMap.set('InputNumber', InputNumber);
+componentMap.set('Switch', Switch);
+componentMap.set('TimePicker', TimePicker);
+componentMap.set('TreeSelect', TreeSelect);
+componentMap.set('Slider', Slider);
+componentMap.set('Rate', Rate);
+componentMap.set('Divider', Divider);
+
+// 属性值
+const props = defineProps(['type','options'])
+const Comp = componentMap.get(props.type) as ReturnType<typeof defineComponent>;
+</script>
+<template>
+  <Comp :options="options"/>
+</template>
--- src/views/doormodel/index.vue
+++ src/views/doormodel/index.vue
@@ -1,35 +1,22 @@
 <script setup lang="ts">
 // 导入文件
-import { aboutPinia } from '@/stores/about';
-import { storeToRefs } from 'pinia';
-import { useRouter } from 'vue-router';
+import { unref, ref, toRef, toRefs,reactive } from 'vue';
+import ReTable from '@/components/base/ReTable/index.vue'
+import BasicForm from '@/components/base/BasicForm/index.vue'
+import {dataSource,columns,formSchema} from './schema'
 
-// 初始变量
-const router = useRouter(); //路由
-const aboutPiniaData = aboutPinia(); //状态数据
-const { message } = storeToRefs(aboutPiniaData);
-// 逻辑部分
-// 更新消息
-const handleUpdateMessage = () => {
-  console.info('我是按钮点击');
-  aboutPiniaData.setNewMessage('我是更新消息的');
-};
-// 重置消息
-const handleResetMessage = () => {
-  aboutPiniaData.$reset();
-};
-// 返回首页
-const handleJumpToHome = () => {
-  router.push('/');
-};
+// 逻辑处理
 </script>
 <template>
-  <div class="material-wrap">
-    <h1>This is an about page</h1>
-    <div>{{ message }}</div>
-    <a-button type="primary" style="margin-top: 20px;" @click="handleUpdateMessage">更新消息</a-button>
-    <a-button type="default" style="margin-top: 20px;" @click="handleResetMessage">重置消息</a-button>
-    <a-button type="default" style="margin-top: 20px;" @click="handleJumpToHome">返回首页</a-button>
+  <div class="user-wrap">
+    <BasicForm :formSchema="formSchema" />
+    <ReTable :dataSource="dataSource" :columns="columns" :tableHeader="true"  >
+     <template #bodyCell="{ column }">
+        <template v-if="column.key === 'operation'">
+          <a-switch checked-children="启用" un-checked-children="禁用" />
+        </template>
+      </template>
+    </ReTable>
   </div>
 </template>
 
