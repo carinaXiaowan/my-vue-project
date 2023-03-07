cb18d0197660e4164322a0672feb36edfdc5f4de
陈晓婉 <chenxiaowan@chenxiaowandeMacBook-Pro.local>
2023-03-07 14:05:32 +0800

    feat:修改文件名称

--- src/views/company/Item.vue
+++ src/views/company/Item.vue
@@ -1,10 +1,10 @@
 <!-- 渲染函数 -->
-<!-- 当组合式 API 与模板一起使用时，setup() 钩子的返回值是用于暴露数据给模板当我们使用渲染函数时，可以直接把渲染函数返回： -->
+<!-- 当组合式 API 与模板一起使用时，setup() 钩子的返回值是用于暴露数据给模板，当我们使用渲染函数时，可以直接把渲染函数返回： -->
 <!-- //文档 https://cn.vuejs.org/guide/extras/render-function.html#custom-directives -->
 <script lang="tsx">
-  export default {
-    setup() {
-      return () => <div>hellossss</div>
-    }
+export default {
+  setup() {
+    return () => <div>hellossss</div>;
   }
+};
 </script>
